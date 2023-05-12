7e5bd29c2e8a1a0497f3d7c49a2bcde40043b1d2
khaliddost <khaliddost@hotmail.com>
2023-05-12 12:39:30 -0400

    New assignment added

---
+++ day01/ChapterOne.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class ChapterOne {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is my first project with my group");
+    }
+}
---
+++ day01/ChapterTwo.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class ChapterTwo {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Chapter two added");
+    }
+}
